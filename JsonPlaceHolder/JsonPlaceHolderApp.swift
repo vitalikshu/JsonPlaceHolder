35926bf05a709301b602324d9785a56a20d33e92
vitalikshu <37334718+vitalikshu@users.noreply.github.com>
2021-02-06 18:59:35 +0200

    Initial Commit

---
+++ JsonPlaceHolder/JsonPlaceHolderApp.swift
@@ -0,0 +1,17 @@
+//
+//  JsonPlaceHolderApp.swift
+//  JsonPlaceHolder
+//
+//  Created by PROG on 06/02/2021.
+//
+
+import SwiftUI
+
+@main
+struct JsonPlaceHolderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
